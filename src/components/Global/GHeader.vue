c6863efa3508efa36424e3681308328a759d4380
Kashirskiy Nikita <lordwebkit@gmail.com>
2024-02-16 08:29:38 +0300

    Init subscription page on tailwind

---
+++ src/components/Global/GHeader.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ILogo from '../../assets/icons/ILogo.vue'
+</script>
+<template>
+  <div class="">
+    <ILogo />
+  </div>
+</template>
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <main class="text-3xl font-bold underline">hi</main>
+</template>
